b2b23e8e4f9b9938ce03ca96b8c62745b6e99172
Dup4 <lyuzhi.pan@gmail.com>
2022-04-17 23:28:27 +0800

    refactor: rename dir

--- test/hello_world_benchmark.cc
+++ test/hello_world_benchmark.cc
@@ -1,6 +1,6 @@
 #include <benchmark/benchmark.h>
 
-#include "algorithmic-library/hello_world.h"
+#include "algorithmic/hello_world.h"
 
 static void BenchmarkHelloWorld(benchmark::State& state) {
     for (auto _ : state) {
--- test/hello_world_test.cc
+++ test/hello_world_test.cc
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#include "algorithmic-library/hello_world.h"
+#include "algorithmic/hello_world.h"
 
 TEST(HELLO_WORLD, hello_world) {
     EXPECT_EQ(HelloWorld(), "Hello World");
