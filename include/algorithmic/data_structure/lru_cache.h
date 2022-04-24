adadd60c77f253332a6a8f789e8ebf8b5704dd09
Dup4 <lyuzhi.pan@gmail.com>
2022-04-24 19:24:44 +0800

    feat: lru cache

---
+++ include/algorithmic/data_structure/lru_cache.h
@@ -0,0 +1,135 @@
+#ifndef ALGORITHMIC_DATA_STRUCTURE_LRU_CACHE_H_
+#define ALGORITHMIC_DATA_STRUCTURE_LRU_CACHE_H_
+
+#include <cstddef>
+#include <memory>
+#include <optional>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+namespace algorithmic::data_structure {
+
+template <typename K, typename V>
+struct DLinkedNode {
+    K key;
+    V value;
+    DLinkedNode* prev;
+    DLinkedNode* next;
+    DLinkedNode() : prev(nullptr), next(nullptr) {}
+    DLinkedNode(K _key, V _value) : key(_key), value(_value), prev(nullptr), next(nullptr) {}
+};
+
+template <typename K, typename V, typename Container = std::unordered_map<K, DLinkedNode<K, V>*>>
+class LRUCache {
+private:
+    using DLinkedNode = DLinkedNode<K, V>;
+
+    Container cache;
+    DLinkedNode* head;
+    DLinkedNode* tail;
+    size_t size;
+    size_t capacity;
+
+public:
+    LRUCache() = delete;
+
+    LRUCache(size_t _capacity) : size(0), capacity(_capacity) {
+        init();
+    }
+
+    ~LRUCache() {
+        clear();
+    }
+
+    std::optional<V> Get(const K& key) {
+        if (!cache.count(key)) {
+            return std::nullopt;
+        }
+
+        DLinkedNode* node = cache[key];
+        moveToHead(node);
+        return node->value;
+    }
+
+    void Put(const K& key, const V& value) {
+        if (!cache.count(key)) {
+            DLinkedNode* node = new DLinkedNode(key, value);
+            cache[key] = node;
+            addToHead(node);
+            ++size;
+            if (size > capacity) {
+                DLinkedNode* removed = removeTail();
+                cache.erase(removed->key);
+                delete removed;
+                --size;
+            }
+        } else {
+            DLinkedNode* node = cache[key];
+            node->value = value;
+            moveToHead(node);
+        }
+    }
+
+    size_t Size() const {
+        return size;
+    }
+
+    void Clear() {
+        clear();
+        init();
+    }
+
+private:
+    void init() {
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    void clear() {
+        for (const auto& [k, v] : cache) {
+            if (v != nullptr) {
+                delete v;
+            }
+        }
+
+        cache.clear();
+
+        if (head != nullptr) {
+            delete head;
+        }
+
+        if (tail != nullptr) {
+            delete tail;
+        }
+    }
+
+    void addToHead(DLinkedNode* node) {
+        node->prev = head;
+        node->next = head->next;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    void removeNode(DLinkedNode* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void moveToHead(DLinkedNode* node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    DLinkedNode* removeTail() {
+        DLinkedNode* node = tail->prev;
+        removeNode(node);
+        return node;
+    }
+};
+
+}  // namespace algorithmic::data_structure
+
+#endif  // ALGORITHMIC_DATA_STRUCTURE_LRU_CACHE_H_
---
+++ test/data_structure/lru_cache_test.cc
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "snapshot/snapshot.h"
+
+#include "algorithmic/data_structure/lru_cache.h"
+
+using namespace std;
+using namespace algorithmic::data_structure;
+
+class LRUCacheTest : public testing::Test {
+protected:
+    virtual void SetUp() override {}
+};
+
+TEST(LRUCacheTest, lru_cache_test) {
+    auto lru_cache = LRUCache<int, int>(5);
+
+    {
+        EXPECT_FALSE(lru_cache.Get(1).has_value());
+
+        for (int i = 1; i <= 5; i++) {
+            lru_cache.Put(i, i);
+            EXPECT_EQ(lru_cache.Get(i).value(), i);
+        }
+
+        for (int i = 6; i <= 10; i++) {
+            lru_cache.Put(i, i);
+            EXPECT_EQ(lru_cache.Get(i), i);
+            EXPECT_FALSE(lru_cache.Get(i - 5).has_value());
+        }
+    }
+}
