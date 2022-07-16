1779f9c4970ef3dfe69694faab9e20ef402a4fea
Dup4 <lyuzhi.pan@gmail.com>
2022-07-16 21:36:47 +0800

    test: add nx case

--- test/algorithmic/string/sub_sequence_automaton_test.cc
+++ test/algorithmic/string/sub_sequence_automaton_test.cc
@@ -22,5 +22,14 @@ TEST(SubSequenceAutomatonTest, sub_sequence_automaton_test) {
 
         EXPECT_TRUE(sub_sequence_automaton.MatchSubSequence("aa"));
         EXPECT_TRUE(sub_sequence_automaton.MatchSubSequence("bb"));
+
+        const auto& nx = sub_sequence_automaton.GetNext();
+
+        EXPECT_EQ(nx[1].nx[0], 7);
+        EXPECT_EQ(nx[1].nx[1], 2);
+        EXPECT_EQ(nx[1].nx[2], 3);
+        EXPECT_EQ(nx[1].nx[3], 4);
+        EXPECT_EQ(nx[1].nx[4], 5);
+        EXPECT_EQ(nx[1].nx[5], 6);
     }
 }
