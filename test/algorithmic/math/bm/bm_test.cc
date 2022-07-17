0dd4ff988b500e9b93c738dc4f3180c8c3e30291
Dup4 <lyuzhi.pan@gmail.com>
2022-07-17 16:10:49 +0800

    test: add BMPrime test case

--- test/algorithmic/math/bm/bm_test.cc
+++ test/algorithmic/math/bm/bm_test.cc
@@ -25,3 +25,50 @@ TEST(BMPrimeTest, bm_prime_test) {
         EXPECT_EQ(res, 726789438);
     }
 }
+
+// https://leetcode.cn/problems/count-the-number-of-ideal-arrays/
+TEST(BMPrimeTest, bm_prime_test_1) {
+    const auto sol = [](int n, int maxValue) {
+        const int N = 100;
+        const int mod = 1000000007;
+
+        auto f = std::vector<std::vector<int>>(N + 1, std::vector<int>(maxValue + 5, 0));
+
+        for (int i = 1; i <= maxValue; i++) {
+            f[1][i] = 1;
+        }
+
+        for (int i = 1; i < N; i++) {
+            auto &cur = f[i];
+            auto &nx = f[i + 1];
+
+            for (int j = 1; j <= maxValue; j++) {
+                for (int k = 1; j * k <= maxValue; k++) {
+                    nx[int(j * k)] += cur[j];
+                    nx[int(j * k)] %= mod;
+                }
+            }
+        }
+
+        auto g = std::vector<long long>();
+
+        for (int i = 1; i <= N; i++) {
+            g.push_back(0);
+            for (int j = 1; j <= maxValue; j++) {
+                g.back() += f[i][j];
+                g.back() %= mod;
+            }
+        }
+
+        if (n <= N) {
+            return g[n - 1];
+        }
+
+        auto bm = algorithmic::math::BMPrime<100000>::Builder().WithMod(mod).Build();
+        return bm.Gao(g, n - 1);
+    };
+
+    EXPECT_EQ(sol(10000, 10000), 22940607);
+    EXPECT_EQ(sol(5000, 10000), 553451492);
+    EXPECT_EQ(sol(9999, 9999), 984922380);
+}
